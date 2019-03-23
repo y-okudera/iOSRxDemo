645b67166631afaee111c406e7171ec2c9c026ea
y.okudera <>
2019-03-24 02:00:26 +0900

    Create project.

---
+++ iOSRxDemo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  iOSRxDemo
+//
+//  Created by YukiOkudera on 2019/03/24.
+//  Copyright © 2019 Yuki Okudera. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ iOSRxDemo/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  iOSRxDemo
+//
+//  Created by YukiOkudera on 2019/03/24.
+//  Copyright © 2019 Yuki Okudera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
